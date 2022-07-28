e4920dc3a5f47e76b8f35a6a41b34c0aaee838b5
Amel <amelntamark@gmail.com>
2022-07-28 12:23:53 -0700

    add tester implementation to genres route

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,10 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     # Register Blueprints here
-    from .routes import genres_bp
+    from .routes import genres_bp, eras_bp, runtime_bp
     app.register_blueprint(genres_bp)
+    app.register_blueprint(eras_bp)
+    app.register_blueprint(runtime_bp)
 
     from .horror_route import horror_bp  # This BP is a test
     app.register_blueprint(horror_bp)
--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,7 @@ TMDB_API_KEY = os.environ.get("TMDB_API_KEY")
 
 genres_bp = Blueprint("genres_bp", __name__, url_prefix="/genres")
 eras_bp = Blueprint("eras_bp", __name__, url_prefix="/eras")
+runtime_bp = Blueprint("runtime_bp", __name__, url_prefix="/runtime")
 
 
 @genres_bp.route("", methods=["POST"])
@@ -20,10 +21,24 @@ def post_genre_preferences():
 
     # Validate data in request body. If valid, add to session database.
 
+    # Basic tester implementation
+    request_body = request.get_json()
+    response_string = f"Recorded your preference for: {request_body['genres']}"
+
+    return response_string
+
 
 @eras_bp.route("", methods=["POST"])
 def post_era_preferences():
     """Posts preferences for release dates (by decades) to MAD API.
     Request should be in JSON format: {"eras": ["1980s", "1990s", "2000s", "2010s", "2020s"]}"""
+
+    # Validate data and add to session database.
+
+
+@runtime_bp.route("", methods=["POST"])
+def post_runtime_preferences():
+    """Posts user's runtime preferences to MAD API.
+    Request should be in JSON format: {"runtime": "< 120 mins"}"""
 
     # Validate data and add to session database.
