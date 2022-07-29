246fc75f96229dfef751673502eb51d77b8fcad1
Amel <amelntamark@gmail.com>
2022-07-29 14:38:28 -0700

    add seen it endpoint

--- app/users_routes.py
+++ app/users_routes.py
@@ -12,25 +12,18 @@ def create_user():
     """Adds new user to user database."""
     request_body = request.get_json()
     new_user = UserData(
-        user_name=request_body['user_name'], seen_it='')
-
-    request_body = request.get_json()
-    new_user = UserData(
-        user_name=request_body['user_name'])
+        user_name=request_body['username'])
 
     db.session.add(new_user)
     db.session.commit()
 
-    return f"User {new_user.user_name} created successfully :)"
-
-    return f"User account for  {new_user.user_name} created."
+    return f"User {new_user.username} created successfully :)"
 
 
 @users_bp.route("/<user_id>/<movie_id>", methods=["PATCH"])
 def add_to_users_seen_list(user_id, movie_id):
     """Adds a movie's TMdB ID to a users seen list"""
 
-    # TODO: Get user's seen list. Append a new movie to it. Return it.
     user = UserData.query.get(user_id)
     seen_list = user.seen_it
 
